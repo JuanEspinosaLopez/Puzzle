eb8f1cb6b9180fd6b6bf104a14597085c4e1218d
Juan Ramon Espinosa Lopez <espinosalopezjuanramon@gmail.com>
2018-11-06 23:18:07 -0600

    Se agregó volley al proyecto

--- app/src/main/java/puzzle/dotjar/com/puzzle/PuzzleOrdenamiento/Comunicador.java
+++ app/src/main/java/puzzle/dotjar/com/puzzle/PuzzleOrdenamiento/Comunicador.java
@@ -3,10 +3,20 @@ package puzzle.dotjar.com.puzzle.PuzzleOrdenamiento;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.webkit.JavascriptInterface;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import puzzle.dotjar.com.puzzle.Database;
+import puzzle.dotjar.com.puzzle.Red;
 
 public class Comunicador
 {
@@ -14,32 +24,48 @@ public class Comunicador
     private int numeroNivel;
     private SQLiteDatabase sqLiteDatabase;
     private Database database;
-
+    private JSONObject puzzle;
     Comunicador(Context contexto, int numeroNivel)
     {
         this.contexto = contexto;
         this.numeroNivel=numeroNivel;
         this.database= new Database(contexto, "puzzle", null, 1);
         this.sqLiteDatabase=database.getReadableDatabase();
+        this.puzzle =new JSONObject();
     }
     
     @JavascriptInterface
-    public String getPuzzle()
+    public String createPuzzle()
     {
-        //TODO: Usar el objeto SqliteDatabase para acceder al puzzle con ID: this.numeroNivel
-        JSONObject jsonObject=new JSONObject();
-        try
+        RequestQueue requestQueue= Volley.newRequestQueue(this.contexto);
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, Red.DIRECCION_IP + "/", new Response.Listener<String>()
         {
-            jsonObject.put("setName_"+0, "print (cada_item)");
-            jsonObject.put("setName_"+1, "print (cada_item2)");
-            jsonObject.put("setName_"+2, "print (cada_item3)");
-            jsonObject.put("setName_"+3, "print (cada_item4)");
-        }
-        catch (JSONException e)
+            @Override
+            public void onResponse(String response)
+            {
+                try
+                {
+                    JSONArray jsonArray=new JSONArray(response);
+                    for(int i=0; i<jsonArray.length(); i++)
+                        agregarLinea(i, jsonArray.getString(i));
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener()
         {
-            e.printStackTrace();
-        }
-        return jsonObject.toString();
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(stringRequest);
+
+        return getPuzzle();
     }
     @JavascriptInterface
     public String getTitulo()
@@ -58,4 +84,25 @@ public class Comunicador
                 "<p>5</p>";
     }
 
+    public void limpiarPuzzle()
+    {
+        this.puzzle=new JSONObject();
+    }
+    public int agregarLinea(int lugar, String linea)
+    {
+        try
+        {
+            this.puzzle.put("set_Name"+lugar, linea);
+            return 1;
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    public String getPuzzle()
+    {
+        return puzzle.toString();
+    }
 }
---
+++ app/src/main/java/puzzle/dotjar/com/puzzle/Red.java
@@ -0,0 +1,6 @@
+package puzzle.dotjar.com.puzzle;
+
+public class Red
+{
+    public static final String DIRECCION_IP="192.168.1.64:80";
+}
